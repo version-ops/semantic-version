60b1a326636abd42a5a313552c8ea37668c156a4
Kurt Guenther <kurtwguenther@gmail.com>
2026-08-15 18:00:11 -0600

    add reckon release plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,25 @@ include(
     "libs:semantic-version",
 )
 
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.ajoberstar.reckon.settings") version "0.19.2"
+}
+
+extensions.configure<org.ajoberstar.reckon.gradle.ReckonExtension> {
+    setDefaultInferredScope("minor")
+    snapshots()
+    setScopeCalc(calcScopeFromProp().or(calcScopeFromCommitMessages()))
+    setStageCalc(calcStageFromProp())
+}
+
 fun ProjectDescriptor.applyGradleKtsBuildFileNames() {
     children.forEach { subproject ->
         subproject.buildFileName = "${subproject.name}.gradle.kts"
